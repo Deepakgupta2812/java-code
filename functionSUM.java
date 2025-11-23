ff4b0b8887d099ca99eae84f52750fefd2b16370
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2025-11-24 01:03:56 +0530

    sum using function

---
+++ functionSUM.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class functionSUM {
+    public static int Sum(int a , int b){
+        int sum=a+b;
+        return sum;
+    }
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a= sc.nextInt();
+       int b= sc.nextInt();
+       int sum=Sum(a, b);
+       System.out.println("Sum of 2 number is = "+sum); 
+    }
+}

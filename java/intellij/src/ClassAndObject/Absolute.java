1a9d2b8d0f16e674e3563e596dd17d27dbbf51a6
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2025-12-09 00:36:11 +0530

    absolute value

---
+++ java/intellij/src/ClassAndObject/Absolute.java
@@ -0,0 +1,13 @@
+package ClassAndObject;
+
+import java.util.Scanner;
+
+public class Absolute {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int num= sc.nextInt();
+        int result=num>=0?num:-num;
+        System.out.println("Absolute value is "+result);
+    }
+}

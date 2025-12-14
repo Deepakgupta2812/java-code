15934984bccc4f6b0a1ba1903ae0f8a431dd60d9
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2025-12-15 00:26:06 +0530

    DiceRoll

---
+++ java/intellij/src/ClassAndObject/DiceRoll.java
@@ -0,0 +1,17 @@
+package ClassAndObject;
+
+public class DiceRoll {
+    int roll(){
+        double random=Math.random()*6;
+       return (int)Math.ceil(random);
+
+    }
+    public static void main(String[] args) {
+        DiceRoll dice=new DiceRoll();
+        for (int i=0;i<10;i++){
+            System.out.println(dice.roll());
+        }
+
+
+    }
+}

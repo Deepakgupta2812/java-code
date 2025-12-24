3f427f9730e8284b6bddfa049ef540bd0755ba14
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2025-12-24 18:46:38 +0530

    string Manipulation

---
+++ java/intellij/src/ClassAndObject/StringManipulation.java
@@ -0,0 +1,11 @@
+package ClassAndObject;
+
+public class StringManipulation {
+    public static void main(String[] args) {
+        String firstName="Deepak";
+        String LastName="Gupta";
+        String FullName=firstName.concat(" ").concat(LastName);
+        System.out.println(FullName.toUpperCase());
+
+    }
+}

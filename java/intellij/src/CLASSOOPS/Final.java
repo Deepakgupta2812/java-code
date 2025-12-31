891a1c71def021e054e18c0543cccc7084fa0f14
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2026-01-01 01:53:35 +0530

    final operator in java

---
+++ java/intellij/src/CLASSOOPS/Final.java
@@ -0,0 +1,11 @@
+//package CLASSOOPS;
+//
+//public class Final {
+//   final double pi=3.1412;
+//    void testing(){
+//        pi=56;
+//    }
+//    public static void main(String[] args) {
+//
+//    }
+//}

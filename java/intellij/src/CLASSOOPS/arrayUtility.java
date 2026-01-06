218c5ac60e471692cdae0bdb4f426106e5d37911
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2026-01-07 01:06:29 +0530

    array utility

---
+++ java/intellij/src/CLASSOOPS/arrayUtility.java
@@ -0,0 +1,27 @@
+package CLASSOOPS;
+
+import java.util.Scanner;
+
+public class arrayUtility {
+    public static  int[] inputArray(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Please enter number of element :");
+        int size=sc.nextInt();
+        int [] nums=new int[size];
+        int i=0;
+        while (i<size){
+            System.out.println("Please enter element no "+(i+1)+":");
+            nums[i]= sc.nextInt();
+            i++;
+        }
+        return nums;
+    }
+    public static void displayarray(int [] numArray){
+        int i=0;
+        while (i<numArray.length){
+            System.out.println(numArray[i]+" ");
+            i++;
+        }
+        System.out.println();
+    }
+}

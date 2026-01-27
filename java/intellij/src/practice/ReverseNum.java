4eb36c018abfeacdccdd9ebd0f5d0d23dc8e6377
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2026-01-27 20:47:02 +0530

    reverse no.

---
+++ java/intellij/src/practice/ReverseNum.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.Scanner;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n= sc.nextInt();
+        int rev=0;
+        int rem;
+        while (n>0){
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+        System.out.println("Reverse number is "+rev);
+    }
+}

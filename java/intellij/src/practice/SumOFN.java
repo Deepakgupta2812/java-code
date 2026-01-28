92664a21e531ec5f96829d204cbda489d3a768f6
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2026-01-28 20:49:54 +0530

    sum of n no.

---
+++ java/intellij/src/practice/SumOFN.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class SumOFN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n= sc.nextInt();
+        int sum=0;
+        int i=1;
+        while (i<=n){
+            sum+=i;
+            i++;
+        }
+        System.out.println("The sum is :"+sum);
+    }
+}

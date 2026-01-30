3494399f46ee401ad375869ff1872deabaa3d76a
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2026-01-30 14:45:58 +0530

    arithmatic

---
+++ java/intellij/src/progarmja/arithmatic.java
@@ -0,0 +1,15 @@
+package progarmja;
+
+import java.util.Scanner;
+
+public class arithmatic {
+    public static void main(String[] args) {
+        System.out.println("welcome to arithmetic calculater\n\n");
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter first number");
+        int first=input.nextInt();
+        System.out.print("enter the second number");
+        int second=input.nextInt();
+        
+    }
+}

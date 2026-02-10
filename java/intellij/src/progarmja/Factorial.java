587603ca97cf657f995d3e7ea0c4d130979eecf9
Deepakgupta2812 <deepakkumarguptasma28@gmail.com>
2026-02-10 15:42:35 +0530

    factorial

---
+++ java/intellij/src/progarmja/Factorial.java
@@ -0,0 +1,20 @@
+package progarmja;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number");
+        int num= sc.nextInt();
+        int fact=1;
+        if (num==0){
+            fact=1;
+        }
+        while (num>0){
+            fact=fact*num;
+            num--;
+        }
+        System.out.println(fact);
+    }
+}

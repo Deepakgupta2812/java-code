726214c92a6f1d4854aad6759d4d323b881659f4
Deepak Gupta <deepakkumarguptasma28@gmail.com>
2026-06-18 19:50:50 +0530

    problem 09

---
+++ 9.palindrome-number.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    private static boolean checkPalindrome(String num){
+        int left =0;
+        int right = num.length()-1;
+        while(left<right){
+            if(num.charAt(left)!= num.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        String num = x+ "";
+        return checkPalindrome(num);
+    }
+
+}
